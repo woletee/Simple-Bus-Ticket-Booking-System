98fd2264d20bc93c80c0ff4a6391a3d1d8ae8b39
woletee <118590747+woletee@users.noreply.github.com>
2023-11-26 00:37:05 +0900

    booking_details.php

---
+++ booking_details.php
@@ -0,0 +1,8 @@
+
+
+<!-- HTML Form -->
+<form method="POST" action="<?php echo $_SERVER['PHP_SELF']; ?>">
+    <label for="customer_id">Enter Customer ID:</label>
+    <input type="text" id="customer_id" name="customer_id" required>
+    <input type="submit" name="submit" value="Retrieve Columns">
+</form>
